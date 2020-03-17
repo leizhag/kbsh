52774cb0929d30e2a5c17b69ccfd198c805cd52d
leizhag <leizhang@gmx.com>
2020-03-17 20:04:15 +0800

    feat: cli arg - add context

--- kubeshell/main.py
+++ kubeshell/main.py
@@ -3,13 +3,23 @@
 
 
 from __future__ import print_function, absolute_import, unicode_literals
-from app import Kubeshell
+
+import argparse
+
+from app import Kubeshell, KubeConfig
 
 import logging
 logger = logging.getLogger(__name__)
 
 
 def cli():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--context', help='The context name to use '
+                                                'when starting the kube-shell.')
+    args = parser.parse_args()
+    if args.context:
+        KubeConfig.switch_context(args.context)
+
     kube_shell= Kubeshell()
     logger.info("session start")
     kube_shell.run_cli()
